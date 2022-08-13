923de82ca4cf4eaf29665c7a37e21124db24d3e1
Yehor Havryliuk <yehor.havryliuk@gmail.com>
2022-08-13 17:33:05 +0300

    Added delete course example

--- src/main/java/com/example/demohibernateapp/jdbc/DeleteCourseDemo.java
+++ src/main/java/com/example/demohibernateapp/jdbc/DeleteCourseDemo.java
@@ -1,12 +1,15 @@
 package com.example.demohibernateapp.jdbc;
 
+import com.example.demohibernateapp.entity.Course;
 import com.example.demohibernateapp.entity.Instructor;
 import com.example.demohibernateapp.entity.InstructorDetail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class BiDirectionalDemo {
+import java.util.List;
+
+public class DeleteCourseDemo {
 
     public static void main(String[] args) {
 
@@ -14,15 +17,16 @@ public class BiDirectionalDemo {
                 .configure()
                 .addAnnotatedClass(Instructor.class)
                 .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
                 .buildSessionFactory();
 
         try (factory; Session session = factory.getCurrentSession()) {
             session.beginTransaction();
 
-            int id = 1;
-            InstructorDetail detail = session.get(InstructorDetail.class, id);
-            Instructor instructor = detail.getInstructor();
-            System.out.println(instructor.getFirstName() + " " + instructor.getLastName());
+            int id = 11;
+            Course course = session.get(Course.class, id);
+
+            session.delete(course);
 
             session.getTransaction().commit();
         } catch (Exception ex) {
